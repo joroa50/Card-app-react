ae247ccc32bdd448e938fb1b309bc23389d3231f
José Rodrigo Roa Acosta <jroa@cosinte.bog>
2024-11-14 15:15:15 -0500

    Inicio de la app funcionando el NavBar

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from "./components/Navbar";
+import "./main.css";
+
+
+
+
+function App() {
+  
+
+  return (
+  
+      <div>
+        <Navbar />
+        
+      </div>
+      
+   
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <a href="#" className='logo'><h1>Jroa</h1></a>
+        <ul className='menu'>
+            <li><a className='menu-link' href="#"></a>Inicio</li>
+            <li><a className='menu-link' href="#"></a>Nosotros</li>
+            <li><a className='menu-link' href="#"></a>Productos</li>
+            <li><a className='menu-link' href="#"></a>Contactos</li>
+                                               
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
